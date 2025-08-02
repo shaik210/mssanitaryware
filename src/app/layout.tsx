81198631636f7ca45bf8b88dba9d2d4cff112a79
shiak <noorshaik210@gmail.com>
2025-08-02 16:42:17 +0530

    Fix global CSS import for Next.js App Router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import 'react-toastify/dist/ReactToastify.css'; 
+// import 'react-toastify/dist/ReactToastify.css'; 
 
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
